7c01970645006a38f2985dac75cec75bd2aef886
zabbius <zabbius@gmail.com>
2020-08-26 12:21:45 +0300

    Cli tool

---
+++ src/RefasmerCliTool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JetBrains.Refasmer.CliTool
+{
+    static class Program
+    {
+        static int Main( string[] args )
+        {
+            return JetBrains.Refasmer.Program.Main(args);
+        }
+    }
+}
