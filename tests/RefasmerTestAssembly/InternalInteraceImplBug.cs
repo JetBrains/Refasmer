5a62bd44c90be20a4929320f5fff66a9f27d613b
Ivan Migalev <ivan.migalev@jetbrains.com>
2025-12-20 17:39:17 +0100

    (#54) Add test for a type explicitly implementing an internal interface
    Co-authored-by: Alexander Ulitin <alexander.a.ulitin@gmail.com>

---
+++ tests/RefasmerTestAssembly/InternalInteraceImplBug.cs
@@ -0,0 +1,15 @@
+﻿namespace RefasmerTestAssembly;
+
+public class Class1 : IInternalInterace<string>
+{
+    void IInternalInterace<string>.Method(string x, Internal2 y)
+    {
+    }
+}
+
+internal class Internal2{}
+
+internal interface IInternalInterace<T>
+{
+    void Method(T x, Internal2 y);
+}
